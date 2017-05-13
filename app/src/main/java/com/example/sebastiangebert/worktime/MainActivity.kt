d5758f372d366d5e93c306fc626c120f5d154968
Sebastian Gebert <achondrias@gmail.com>
2017-05-13 16:57:31 +0200

    Create: added library Joda-Time for date and time types. Because the Java types obviously suck and you need a library for it. Also using some Kotlin shenanigans.

--- app/src/main/java/com/example/sebastiangebert/worktime/MainActivity.kt
+++ app/src/main/java/com/example/sebastiangebert/worktime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.sebastiangebert.worktime
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         val db = Database(this).writableDatabase
+        val entries = MutableList(0, { Pair(LocalDate(), LocalTime()) })
 
         db.use {
             db.execSQL("INSERT INTO TimeLogEntry DEFAULT VALUES")
             val allTimeLogEntries = db.query("TimeLogEntry", null, null, null, null, null, null, null)
-            var resultTable = ""
 
             allTimeLogEntries.use {
                 while (allTimeLogEntries.moveToNext()) {
-                    val date = allTimeLogEntries.getString(allTimeLogEntries.getColumnIndex("Date"))
-                    val time = allTimeLogEntries.getString(allTimeLogEntries.getColumnIndex("Time"))
-                    resultTable += "$date\t$time\n"
+                    val date = LocalDate(allTimeLogEntries.getString(allTimeLogEntries.getColumnIndex("Date")))
+                    val time = LocalTime(allTimeLogEntries.getString(allTimeLogEntries.getColumnIndex("Time")))
+                    entries.add(Pair(date, time))
                 }
             }
-
-            text.text = resultTable
         }
+
+        text.text = entries
+                .map { dateTime -> "${dateTime.first}\t${dateTime.second}" }
+                .fold("", { acc, curr -> "$acc\n$curr" })
     }
 }
