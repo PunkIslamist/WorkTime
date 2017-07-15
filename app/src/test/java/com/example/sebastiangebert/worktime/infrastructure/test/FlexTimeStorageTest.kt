1fde7964bce4ca3b0e38e215a0469055cf6a74e4
PunkIslamist <achondrias@gmail.com>
2017-07-15 17:20:00 +0200

    Create: first unit tests for FlexTimeStorage.

--- app/src/test/java/com/example/sebastiangebert/worktime/infrastructure/test/FlexTimeStorageTest.kt
+++ app/src/test/java/com/example/sebastiangebert/worktime/infrastructure/test/FlexTimeStorageTest.kt
@@ -4,25 +4,50 @@ import com.example.sebastiangebert.worktime.infrastructure.Database
 import com.example.sebastiangebert.worktime.infrastructure.FlexTimeStorage
 import org.joda.time.DateTime
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class FlexTimeStorageTest {
-    class MockDataBase(private val entries: MutableCollection<String>) : Database {
+    companion object mockDB : Database {
+        val entries: MutableCollection<String> = mutableListOf()
+
         override fun select() = entries
 
         override fun insert(values: Iterable<String>) {
             entries.addAll(values)
         }
+    }
 
+    @Before
+    fun resetDB() = mockDB.entries.clear()
+
+    @Test
+    fun NoEntries_GetAllGetsNone() {
+        val expected = arrayOf<DateTime>()
+
+        val actual = FlexTimeStorage(mockDB).toTypedArray()
+
+        Assert.assertArrayEquals(expected, actual)
     }
 
     @Test
     fun OneEntry_GetAllGetsOne() {
         val entry = DateTime(0)
-        val testInstance = FlexTimeStorage(MockDataBase(mutableListOf(entry.toString())))
         val expected = mutableListOf(entry).toTypedArray()
+        mockDB.entries.add(entry.toString())
 
-        val actual = testInstance.toTypedArray()
+        val actual = FlexTimeStorage(mockDB).toTypedArray()
+
+        Assert.assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun MultipleEntries_GetAllGetsAll() {
+        val entries = List(123, { DateTime(it.toLong()) })
+        val expected = entries.toTypedArray()
+        mockDB.entries.addAll(entries.map { it.toString() })
+
+        val actual = FlexTimeStorage(mockDB).toTypedArray()
 
         Assert.assertArrayEquals(expected, actual)
     }
